6d8b138d9d9005d92cb53c720b4e0f8b91141dce
hypocrite26 <126610833+hypocrite26@users.noreply.github.com>
2025-01-14 10:13:33 -0500

    feat: Add __init__ functionality

--- llamachain/__init__.py
+++ llamachain/__init__.py
@@ -55,3 +55,5 @@ from llamachain.log import get_logger, setup_logging
 # Updated in commit 26 - 2025-04-05 16:06:18
 
 # Updated in commit 2 - 2025-04-05 17:11:37
+
+# Updated in commit 10 - 2025-04-05 17:11:37
