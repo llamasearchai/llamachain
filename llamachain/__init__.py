84db823a7cc9219bb89973aae0bb7cd6ee146018
hypocrite26 <126610833+hypocrite26@users.noreply.github.com>
2025-03-17 09:13:30 -0400

    test: Improve tests for __init__

--- llamachain/__init__.py
+++ llamachain/__init__.py
@@ -67,3 +67,5 @@ from llamachain.log import get_logger, setup_logging
 # Updated in commit 10 - 2025-04-05 17:44:28
 
 # Updated in commit 18 - 2025-04-05 17:44:28
+
+# Updated in commit 26 - 2025-04-05 17:44:28
