20fd70864e6470171c153d331ad02f6b67700193
hypocrite26 <126610833+hypocrite26@users.noreply.github.com>
2025-03-17 10:16:35 -0400

    chore: Adjust __init__.py configuration

--- llamachain/__init__.py
+++ llamachain/__init__.py
@@ -83,3 +83,5 @@ from llamachain.log import get_logger, setup_logging
 # Updated in commit 10 - 2025-04-05 18:47:34
 
 # Updated in commit 18 - 2025-04-05 18:47:34
+
+# Updated in commit 26 - 2025-04-05 18:47:34
